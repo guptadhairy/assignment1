f64f684f2b7ab65cf1828d45f773fada3ebdf905
DHAIRYA GUPTA <122604403+guptadhairy@users.noreply.github.com>
2024-05-12 23:27:27 +0530

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import google from "../logos/google.png"
-import facebook from "../logos/facebook.png"
-import pling from "../logos/gr.png"
+import google from "../logos/google.png";
+import facebook from "../logos/facebook.png";
+import pling from "../logos/gr.png";
 const Card = () => {
   return (
     <div>
       <div className="w-96 p-10 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8">
         <form className="space-y-6" action="#">
           <div className="flex flex-col items-center justify-center">
-            <img src={pling} alt="Pling" height={"60px"} width={"60px"}/>
+            <img src={pling} alt="Pling" height={"60px"} width={"60px"} />
             <h5 className="text-xl font-medium text-sky-800">Pling</h5>
             <h5 className="text-xs font-medium text-teal-400">
               Connect . Play . Thrive
@@ -41,7 +41,6 @@ const Card = () => {
                 class="w-5 h-5 text-teal-400 "
                 viewBox="0 0 26 24"
                 fill="currentColor"
-               
               >
                 <path d="M12 2C9.243 2 7 4.243 7 7v3H6a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2v-8a2 2 0 00-2-2h-1V7c0-2.757-2.243-5-5-5zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7zm4 10.723V20h-2v-2.277a1.993 1.993 0 01.567-3.677A2.001 2.001 0 0114 16a1.99 1.99 0 01-1 1.723z" />
               </svg>
@@ -77,16 +76,12 @@ const Card = () => {
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="w-full text-white bg-gradient-to-r from-blue-400 to-purple-500 hover:from-blue-800 hover:to-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Login
           </button>
-          <div
-            class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-200 dark:after:border-neutral-200">
-            <p
-              class="mx-4 mb-0 text-center">
-              Or
-            </p>
+          <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-200 dark:after:border-neutral-200">
+            <p class="mx-4 mb-0 text-center">Or</p>
           </div>
           <div className="flex justify-center items-center gap-5">
             <img src={google} alt="Google" height={"35px"} width={"35px"} />
